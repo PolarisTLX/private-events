6b84beb3e6fde7c41362f2f572e079fdbc1dceea
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-05-08 12:05:16 -0400

    Basic login and logout
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
+    else
+      flash.now[:danger] = "Invalid login credentials"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to login_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  get '/signup', to: 'users#new'
+  get 'sessions/new'
+  get     '/signup',  to: 'users#new'
+  get     '/login',   to: 'sessions#new'
+  post    '/login',   to: 'sessions#create'
+  delete  '/logout',  to: 'sessions#destroy'
 
 
   resources :users
