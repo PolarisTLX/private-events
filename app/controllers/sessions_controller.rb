f827f62be22d61d375cb9f0173197c0bd648f627
PolarisTLX <paul.a.rail@gmail.com>
2018-05-08 15:17:48 -0400

    fixed login routing when already logged in
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
+
+  # to check if user is already logged in, so they try to log in again, re-direct them to their profile page.
+  # filter for only "new" when user tried to log in again
+  before_action :already_logged_in, only: [:new]
+
   def new
+
   end
 
   def create
@@ -9,7 +15,7 @@ class SessionsController < ApplicationController
       log_in user
 
       # if usesr checks off the "remember me?" box? 1 : else 0
-      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
 
       redirect_to user
     else
@@ -21,5 +27,13 @@ class SessionsController < ApplicationController
   def destroy
     log_out if logged_in?
     redirect_to login_path
+  end
+
+  private
+
+  def already_logged_in
+    if logged_in?
+      redirect_to current_user
+    end
   end
 end
