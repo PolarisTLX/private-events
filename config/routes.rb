65dbbd965e0ec478805581b5169eab57c5b777cc
PolarisTLX <paul.a.rail@gmail.com>
2018-05-15 13:17:01 -0400

    created about and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  # get 'static_pages/about'
+  # get 'static_pages/contact'
+  get '/about',  to: 'static_pages#about'
+  get '/contact',  to: 'static_pages#contact'
   root    'sessions#new'
   get     'sessions/new'
   get     '/signup',  to: 'users#new'
