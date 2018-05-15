cc687a2352e9048643c38d70a1e20020417100d8
PolarisTLX <paul.a.rail@gmail.com>
2018-05-15 16:12:41 -0400

    modify migration for postgres for Heroku

--- db/migrate/20180508192252_create_events.rb
+++ db/migrate/20180508192252_create_events.rb
@@ -5,7 +5,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
       t.text :description
       t.string :location
       t.date :date
-      t.references :host, foreign_key: true
+      # t.references :host, foreign_key: true
+      t.integer :host_id, index: true 
 
       t.timestamps
     end
