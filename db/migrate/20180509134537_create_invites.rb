e95af0e0932193205b720666b08be4375280c176
PolarisTLX <paul.a.rail@gmail.com>
2018-05-15 16:18:51 -0400

    Migration changes for Heroku

--- db/migrate/20180509134537_create_invites.rb
+++ db/migrate/20180509134537_create_invites.rb
@@ -2,9 +2,11 @@ class CreateInvites < ActiveRecord::Migration[5.2]
   def change
     create_table :invites do |t|
       # t.references :event, foreign_key: true
-      t.references :attended_event, foreign_key: true
+      # t.references :attended_event, foreign_key: true
+      t.integer :attended_event_id, index: true
       # t.references :user, foreign_key: true
-      t.references :attendee, foreign_key: true
+      # t.references :attendee, foreign_key: true
+      t.integer :attendee_id, index: true
       t.boolean :accepted
 
       t.timestamps
